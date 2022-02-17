cedc2623a1df89c2d93006f801db491f9c60c01b
tereesaalvarez <93209324+tereesaalvarez@users.noreply.github.com>
2022-02-17 23:08:34 +0100

    commit

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,12 +1,14 @@
 class media_notas:
+    
+        self.nota1=nota1
+        self.nota2=nota2
+        self.nota3=nota3
     def media_aritmetica:
-        return (self.nota1+self.nota2+self.nota3)/3
-    self.nota1=nota1
-    self.nota2=nota2
-    self.nota3=nota3
-
+            return (self.nota1+self.nota2+self.nota3)/3
     def media_ponderada:
         return round(media_aritmetica)
+
+def introducir_nota:
 
 
 
